5dcbb6dc2d56af96d43f565a65b77b737794beea
gaofeng222 <596642721@qq.com>
2024-04-26 15:12:38 +0800

    fix:完成日期选择赋值

--- src/page/Month/index.jsx
+++ src/page/Month/index.jsx
@@ -2,13 +2,22 @@ import { NavBar, Toast, DatePicker } from "antd-mobile";
 import { UpOutline, DownOutline } from "antd-mobile-icons";
 import { useState } from "react";
 import "./index.scss";
+import dayjs from "dayjs";
 
 const now = new Date();
 const Month = () => {
   const [visible, setVisible] = useState(false);
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs(new Date()).format("YYYY-MM");
+  });
   const toggleDateModal = () => {
     setVisible(true);
   };
+  const handleConfirm = (val) => {
+    const date = dayjs(val).format("YYYY-MM");
+    setCurrentDate(date);
+  };
+
   return (
     <>
       <div className="monthlyBill">
@@ -18,7 +27,7 @@ const Month = () => {
         <div className="content">
           <div className="header">
             <div className="date">
-              <span className="text">2023 | 3月账单</span>
+              <span className="text">{currentDate + ""}月账单</span>
               <span className="arrow" onClick={toggleDateModal}>
                 {visible ? <UpOutline /> : <DownOutline />}
               </span>
@@ -40,13 +49,12 @@ const Month = () => {
             <DatePicker
               title="记账日期"
               visible={visible}
+              precision="month"
               onClose={() => {
                 setVisible(false);
               }}
               max={now}
-              onConfirm={(val) => {
-                Toast.show(val.toDateString());
-              }}
+              onConfirm={handleConfirm}
             />
           </div>
         </div>
