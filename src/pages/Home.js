960c58f831527edc90cd46f427326fbef2faec40
gaofeng222 <596642721@qq.com>
2024-11-01 19:07:11 +0800

    add:完善下拉加载...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import { Button, Swiper, Toast, Image, DotLoading, Space } from "antd-mobile";
 import { http } from "../api/request";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { handleAdd, handleMinus } from "../store/reducer/store";
 import SkeletonAgain from "../components/SkeletonAgain";
@@ -19,7 +19,7 @@ function Home() {
   const [hotLists, setHotLists] = useState([]);
   const [newsList, setNewsList] = useState([]);
   const colors = ["#ace0ff", "#bcffbd", "#e4fabd", "#ffcfac"];
-
+  const loadRef = useRef(null);
   useEffect(() => {
     (async () => {
       const data = await getSwiperData("/hot");
@@ -36,6 +36,19 @@ function Home() {
         setNewsList(data.data.list);
       }
     })();
+
+    const listContainer = new IntersectionObserver((load) => {
+      console.log("🚀 ~ listContainer ~ load:", load);
+      const changes = load[0];
+      if (changes.isIntersecting) {
+        alert("我加载了");
+      }
+    });
+    listContainer.observe(loadRef.current);
+
+    return () => {
+      listContainer.unobserve();
+    };
   }, []);
 
   const items = hotLists.map((item, index) => (
@@ -75,7 +88,7 @@ function Home() {
           return <NewsItem item={item} key={item.id} />;
         })}
       </NesListContainer>
-      <LoadMoreContainer className="loadMore">
+      <LoadMoreContainer className="loadMore" ref={loadRef}>
         <Space block justify="center" align="center">
           数据加载中
           <DotLoading />
